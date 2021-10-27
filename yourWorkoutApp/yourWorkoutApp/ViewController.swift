5ca2ba641a0d93ae0f5d41e563bd51ea05e4ddcb
VyakulinSA <VyakulinSA@gmail.com>
2021-10-27 22:58:56 +0400

    init: initial commit

---
+++ yourWorkoutApp/yourWorkoutApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  yourWorkoutApp
+//
+//  Created by Вякулин Сергей on 27.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
